665f6c8da86d17553a99a27e9562f91e96a2196a
Jason <jingyangsddx@163.com>
2017-11-22 22:31:04 +0800

    v2.3.2 modify codes of moving files

--- src/com/jing/app/jjgallery/server/GdbServlet.java
+++ src/com/jing/app/jjgallery/server/GdbServlet.java
@@ -2,11 +2,9 @@ package com.jing.app.jjgallery.server;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import com.jing.app.jjgallery.http.bean.data.DownloadItem;
 import com.jing.app.jjgallery.http.bean.response.GdbMoveResponse;
 import com.jing.app.jjgallery.http.bean.request.GdbRequestMoveBean;
 import com.jing.app.jjgallery.conf.Command;
@@ -14,6 +12,8 @@ import com.jing.app.jjgallery.conf.Configuration;
 import com.jing.app.jjgallery.conf.Filters;
 
 public class GdbServlet extends BaseJsonServlet<GdbRequestMoveBean, GdbMoveResponse> {
+
+	private String addedPath;
 
 	@Override
 	protected Class<GdbRequestMoveBean> getRequestClass() {
@@ -27,12 +27,8 @@ public class GdbServlet extends BaseJsonServlet<GdbRequestMoveBean, GdbMoveRespo
 
 	private void doRequestMove(GdbRequestMoveBean requestBean, HttpServletResponse resp) throws IOException {
 
-		if (requestBean.getDownloadList() != null) {
-			for (int i = 0; i < requestBean.getDownloadList().size(); i ++) {
-				DownloadItem item = requestBean.getDownloadList().get(i);
-				moveFile(requestBean.getType(), getFilePath(requestBean.getType(), item.getName(), item.getKey()), item.getKey());
-			}
-		}
+		moveAllFiles(requestBean.getType());
+
 		removeEmptyFolders(requestBean.getType());
 		GdbMoveResponse responseBean = new GdbMoveResponse();
 		responseBean.setSuccess(true);
@@ -49,48 +45,60 @@ public class GdbServlet extends BaseJsonServlet<GdbRequestMoveBean, GdbMoveRespo
 		sendResponse(resp, responseBean);
 	}
 
-	private String getFilePath(String type, String filename, String parent) throws UnsupportedEncodingException {
-		String path = null;
+	private void moveAllFiles(String type) {
+		String folderPath;
 		if (type.equals(Command.TYPE_RECORD)) {
-			filename = new String(filename.getBytes("ISO-8859-1"), "utf-8");
-			if (parent == null) {
-				path = Configuration.getRecordPath(getServletContext()) + "/" + filename;
+			folderPath = Configuration.getRecordPath(getServletContext());
+		}
+		else {
+			folderPath = Configuration.getStarPath(getServletContext());
+		}
+		addedPath = folderPath + "/" + Filters.FOLDER_ADDED;
+
+		File[] files = new File(folderPath).listFiles();
+		// 第一级有过滤目录，并且如果没有目录的话直接在added里面创建目录
+		for (File file:files) {
+			if (file.isDirectory() && !Filters.isAvailableFolder(file.getName())) {
+				continue;
+			}
+			if (file.isDirectory()) {
+				move(file, "");
 			}
 			else {
-				parent = Configuration.getRecordPath(getServletContext()) + "/" + parent;
-				path = parent + "/" + filename;
+				String name = file.getName().substring(0, file.getName().lastIndexOf("."));
+				File folder = new File(addedPath + "/" + name);
+				if (!folder.exists()) {
+					folder.mkdir();
+				}
+				moveFile(file, folder.getPath());
 			}
 		}
-    	else if (type.equals(Command.TYPE_STAR)) {
-    	    filename = new String(filename.getBytes("ISO-8859-1"), "utf-8");
-			if (parent == null) {
-				path = Configuration.getStarPath(getServletContext()) + "/" + filename;
-			}
-			else {
-				parent = Configuration.getStarPath(getServletContext()) + "/" + parent;
-				path = parent + "/" + filename;
-			}
-		}
-		return path;
 	}
 
-	private void moveFile(String type, String path, String parent) {
-		File src = new File(path);
-		String targetFolder = Configuration.getStarPath(getServletContext());
-		if (type.equals(Command.TYPE_RECORD)) {
-			targetFolder = Configuration.getRecordPath(getServletContext());
+	private void move(File file, String parent) {
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			for (File f:files) {
+				move(f, parent.concat("/").concat(file.getName()));
+			}
 		}
-		targetFolder = targetFolder + "/" + Filters.FOLDER_ADDED + "/";
-		if (parent != null) {
-			targetFolder = targetFolder + parent + "/";
+		else {
+			moveFile(file, addedPath + parent);
+		}
+	}
+
+	private void moveFile(File src, String targetFolder) {
+		if (targetFolder != null) {
 			File folder = new File(targetFolder);
 			if (!folder.exists()) {
 				folder.mkdirs();
 			}
 		}
-		File target = new File(targetFolder + src.getName());
+		File target = new File(targetFolder + "/" + src.getName());
 		if (target.exists()) {
-			target = new File(targetFolder + "[repeat][" + System.currentTimeMillis() + "]" + src.getName());
+			String name = src.getName().substring(0, src.getName().lastIndexOf("."));
+			String extra = src.getName().substring(src.getName().lastIndexOf(".") + 1);
+			target = new File(targetFolder + "/" + name + "_" + System.currentTimeMillis() + "." + extra);
 		}
 		System.out.println("[GdbServlet]moveFile src:" + src.getPath() + ", target:" + target.getPath());
 		src.renameTo(target);
